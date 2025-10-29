b00a5e5d58cf634349c7fe958725adb91bc8c5ad
Sneha-Nahak <152892843+Sneha-Nahak@users.noreply.github.com>
2025-10-29 19:47:22 +0530

    made some changes

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -19,7 +19,7 @@ export const UserProvider = ({ children }) => {
   }, []);
 
   const login = async (email, password) => {
-    const res = await axios.post("http://localhost:5000/api/users/login", { email, password });
+    const res = await axios.post("https://resume-up-backend-1.onrender.com/api/users/login", { email, password });
     localStorage.setItem("token", res.data.token);
     localStorage.setItem("user", JSON.stringify(res.data));
     axios.defaults.headers.common["Authorization"] = `Bearer ${res.data.token}`;
@@ -27,7 +27,7 @@ export const UserProvider = ({ children }) => {
   };
 
   const register = async (name, email, password) => {
-    const res = await axios.post("http://localhost:5000/api/users/register", { name, email, password });
+    const res = await axios.post("https://resume-up-backend-1.onrender.com/api/users/register", { name, email, password });
     localStorage.setItem("token", res.data.token);
     localStorage.setItem("user", JSON.stringify(res.data));
     axios.defaults.headers.common["Authorization"] = `Bearer ${res.data.token}`;
